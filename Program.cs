4257b76095eb46889775239d504586e7a8574a1d
EllenSamanta <ellen.samanta@outlook.com>
2026-01-29 15:14:56 -0300

    chore: implementando classe abstrata

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+var celularIphone = new Iphone("98313247", "13", "435454", 46);
+
+celularIphone.InstalarAplicativo("whatsapp");
+
+var celularNokia = new Iphone("63050204", "versão 2009", "564533", 100);
+
+celularNokia.InstalarAplicativo("agenda");
+
+
+celularIphone.Ligar();
+
